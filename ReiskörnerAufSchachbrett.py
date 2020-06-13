94f608afbba0bdd607a721ab72ca7f6a8d6deab2
andeuw <sendwk@online.de>
2020-06-13 22:06:29 +0200

    Grafische Darstellung der Reiskornmenge

--- ReiskörnerAufSchachbrett.py
+++ ReiskörnerAufSchachbrett.py
@@ -1,14 +1,20 @@
+import matplotlib.pyplot as plt
+
 summe = 0
+feldliste = []
 
 for feld in range(64):
     reiskorn = 2**feld
+    feldliste.append(reiskorn)
     summe += reiskorn
-    print("In Feld {} sind = {:>30,} \
+    print(f"In Feld {feld+1} sind = {reiskorn:>30,} \
     Reiskörner und damit auf dem Schachbrett insgesamt \
-    {:>30,} Reiskörner".format(feld+1, reiskorn, summe))
+    {summe:>30,} Reiskörner")
 
-gewicht = summe * 0.25 / 1000000
+gewicht = summe * 0.025 / 1000000
 print()
-print('Wenn ein Reiskorn 0,25 Gramm wiegt, \
-wiegen die gesammten Reiskörner {:,.0f} \
-Tonnen'.format(gewicht))
+print(f'Wenn ein Reiskorn 0,025 Gramm wiegt, \
+wiegen die gesammten Reiskörner {gewicht:,.0f} Tonnen')
+
+plt.plot(feldliste)
+plt.show()
