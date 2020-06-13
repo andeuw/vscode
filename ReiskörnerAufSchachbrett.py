fcb259edfdbaf9d04ce7a0406f3a6797a35edbae
andeuw <sendwk@online.de>
2020-06-13 20:33:57 +0200

    gewichtsberechnung formiert

--- ReiskörnerAufSchachbrett.py
+++ ReiskörnerAufSchachbrett.py
@@ -1,5 +1,14 @@
 summe = 0
+
 for feld in range(64):
     reiskorn = 2**feld
     summe += reiskorn
-    print("In Feld {} sind = {} Reiskörner und damit auf dem Schachbrett insgesamt {} Reiskörner".format(feld+1, reiskorn, summe))
+    print("In Feld {} sind = {:>30,} \
+    Reiskörner und damit auf dem Schachbrett insgesamt \
+    {:>30,} Reiskörner".format(feld+1, reiskorn, summe))
+
+gewicht = summe * 0.25 / 1000000
+print()
+print('Wenn ein Reiskorn 0,25 Gramm wiegt, \
+wiegen die gesammten Reiskörner {:,.0f} \
+Tonnen'.format(gewicht))
