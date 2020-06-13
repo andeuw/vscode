bbd41d5b5b0d2acea49db0e07a3de91ccd5f3b03
andeuw <sendwk@online.de>
2020-06-14 00:10:31 +0200

    Vollständig

--- kreiszahlPi.py
+++ kreiszahlPi.py
@@ -1,22 +1,25 @@
 import turtle as tu
 
-lines = 100
+lines = 100_000
 
-with open('pi.txt',"r") as f:
-    pi = f.read()
-
-# Länge der (Nachkommastellen-1) und der ersten und letzten 10 Ziffern
-print(len(pi))
-print(pi[1:11])
-print(pi[-11:])
+with open("pi.txt", "r") as f:
+  pi = f.read()
 
 tu.mode('logo')
 tu.tracer(False)
+tu.screensize(6000, 6000, 'black')
+tu.colormode(255)
+
 
 for n in range(lines):
-    zahl = int(pi[n])
-    rotation = zahl * 36
-    tu.setheading(rotation)
-    tu.forward(30)
-    
+  color = int(n/(lines/255))
+  tu.pencolor(255, 255-color, color)
+  zahl = int(pi[n])
+  rotation = zahl * 36
+  tu.setheading(rotation)
+  tu.forward(3)
+  if n % 10_000 == 0:
+    tu.update()
+
+tu.getcanvas().postscript(file='PI_Picture.ps')
 tu.done()
